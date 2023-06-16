e4ce4b03942e95c69e61e34c5ec88d34a845f9f5
Zidane Innis <zidaneinnis@gmail.com>
2023-06-15 21:17:21 -0500

    Getting More Direct Element Access via then()

--- cypress/e2e/contact.cy.js
+++ cypress/e2e/contact.cy.js
@@ -6,10 +6,14 @@ describe('contact form', function () {
 		cy.get('[data-cy="contact-input-message"]').type('Hi my name is Karson');
 		cy.get('[data-cy="contact-input-name"]').type('Zidane');
 		cy.get('[data-cy="contact-input-email"]').type('zidaneinnis@email.com');
+		cy.get('[data-cy="contact-btn-submit"]').then((el) => {
+			expect(el.attr('disabled')).to.be.undefined;
+			expect(el.text()).to.be('Send Message');
+		});
 		cy.get('[data-cy="contact-btn-submit"]').as('submitBtn');
-		cy.get('@submitBtn')
-			.contains('Send Message')
-			.should('not.have.attr', 'disabled');
+		// cy.get('@submitBtn')
+		// 	.contains('Send Message')
+		// 	.should('not.have.attr', 'disabled');
 		cy.get('@submitBtn').click();
 		cy.get('@submitBtn').contains('Sending...');
 		cy.get('@submitBtn').should('have.attr', 'disabled');
