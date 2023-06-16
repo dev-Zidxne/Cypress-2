f1f3c3706b3dd1942579676a56684dcc6a2b1b0d
Zidane Innis <zidaneinnis@gmail.com>
2023-06-15 20:11:36 -0500

    Testing page navigation

--- cypress/e2e/navigation.cy.js
+++ cypress/e2e/navigation.cy.js
@@ -3,6 +3,12 @@
 describe('page naviagtion', function () {
 	it('should navigate between pages', function () {
 		cy.visit('localhost:5173/');
-		cy.get('[data-cy="header-about-link"]');
+		cy.get('[data-cy="header-about-link"]').click();
+		cy.location('pathname').should('eq', '/about'); // / about => About Page
+		cy.go('back');
+		cy.location('pathname').should('eq', '/'); // /  =. Home Page
+		cy.get('[data-cy="header-about-link"]').click();
+		cy.get('[data-cy="header-home-link"]').click();
+		cy.location('pathname').should('eq', '/'); // /  =. Home Page
 	});
 });
