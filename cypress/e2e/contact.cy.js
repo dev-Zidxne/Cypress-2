6fdd3fa855b1e5fdc57f38b7947c2af524097fe8
Zidane Innis <zidaneinnis@gmail.com>
2023-06-15 20:38:22 -0500

    Repetition and More Assertions

---
+++ cypress/e2e/contact.cy.js
@@ -0,0 +1,18 @@
+/// <reference types="Cypress" />
+
+describe('contact form', function () {
+	it('should submit the form', function () {
+		cy.visit('http://localhost:5173/about');
+		cy.get('[data-cy="contact-input-message"]').type('Hi my name is Karson');
+		cy.get('[data-cy="contact-input-name"]').type('Zidane');
+		cy.get('[data-cy="contact-input-email"]').type('zidaneinnis@email.com');
+		cy.get('[data-cy="contact-btn-submit"]').contains('Send Message');
+		cy.get('[data-cy="contact-btn-submit"]').should(
+			'not.have.attr',
+			'disabled'
+		);
+		cy.get('[data-cy="contact-btn-submit"]').click();
+		cy.get('[data-cy="contact-btn-submit"]').contains('Sending...');
+		cy.get('[data-cy="contact-btn-submit"]').should('have.attr', 'disabled');
+	});
+});
