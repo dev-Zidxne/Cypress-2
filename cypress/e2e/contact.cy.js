9c6a410a49fa91f228c01792080f6176cce15fde
Zidane Innis <zidaneinnis@gmail.com>
2023-06-15 21:25:41 -0500

    Simulating Special Key Presses

--- cypress/e2e/contact.cy.js
+++ cypress/e2e/contact.cy.js
@@ -5,16 +5,18 @@ describe('contact form', function () {
 		cy.visit('http://localhost:5173/about');
 		cy.get('[data-cy="contact-input-message"]').type('Hi my name is Karson');
 		cy.get('[data-cy="contact-input-name"]').type('Zidane');
-		cy.get('[data-cy="contact-input-email"]').type('zidaneinnis@email.com');
 		cy.get('[data-cy="contact-btn-submit"]').then((el) => {
 			expect(el.attr('disabled')).to.be.undefined;
-			expect(el.text()).to.be('Send Message');
+			expect(el.text()).to.eq('Send Message');
 		});
+		cy.get('[data-cy="contact-input-email"]').type(
+			'zidaneinnis@email.com{enter}'
+		);
 		cy.get('[data-cy="contact-btn-submit"]').as('submitBtn');
 		// cy.get('@submitBtn')
 		// 	.contains('Send Message')
 		// 	.should('not.have.attr', 'disabled');
-		cy.get('@submitBtn').click();
+		// cy.get('@submitBtn').click();
 		cy.get('@submitBtn').contains('Sending...');
 		cy.get('@submitBtn').should('have.attr', 'disabled');
 	});
